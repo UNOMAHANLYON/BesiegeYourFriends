77987d74ec74ea3766b44aad6dddd31c5234d436
UNOMAHANLYON <nlyon@unomaha.edu>
2018-04-14 14:08:30 -0500

    added sprite class outline

--- src/object/Sprite.java
+++ src/object/Sprite.java
@@ -1,4 +1,38 @@
 package object;
 
+import bounding.BoundingShapes;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Sprite {
+
+    private BoundingShapes outterBounds;
+    private BoundingShapes innerBounds;
+    private BufferedImage spriteImage;
+
+    public Sprite(){
+
+
+
+    }
+
+    public void render( Graphics G) {
+
+
+
+    }
+
+    public void showBounds() {
+
+
+
+    }
+
+    public void Update( float rotate, float scale, float tx, float ty ){
+
+
+
+    }
+
 }
