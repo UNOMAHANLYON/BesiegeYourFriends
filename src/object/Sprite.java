81e3a4a22d7d8c6e320d80040e77c96e94861b58
UNOMAHANLYON <nlyon@unomaha.edu>
2018-04-14 14:20:31 -0500

    fleshed out some sprite class functions

--- src/object/Sprite.java
+++ src/object/Sprite.java
@@ -1,7 +1,9 @@
 package object;
 
 import bounding.BoundingShapes;
+import util.Matrix3x3f;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -10,14 +12,26 @@ public class Sprite {
     private BoundingShapes outterBounds;
     private BoundingShapes innerBounds;
     private BufferedImage spriteImage;
+    private String path;
 
-    public Sprite(){
+    private Matrix3x3f world;
 
+    public Sprite( String path ){
 
+        this.path = path;
+        try {
+
+            spriteImage = ImageIO.read(getClass().getResource(path));
+
+        } catch ( Exception e ) {
+
+            e.printStackTrace();
+
+        }
 
     }
 
-    public void render( Graphics G) {
+    public void render( Graphics G ) {
 
 
 
@@ -29,7 +43,9 @@ public class Sprite {
 
     }
 
-    public void Update( float rotate, float scale, float tx, float ty ){
+    public void Update( float rotate, float scale, float tx, float ty, float deltaTime ){
+
+        world =  Matrix3x3f.identity();
 
 
 
