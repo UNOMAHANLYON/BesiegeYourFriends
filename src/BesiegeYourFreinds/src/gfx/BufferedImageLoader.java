c8cc930f17b078d1f92f549adbf5145d4389da18
Jamie Guenther <jamieguenther@unomaha.edu>
2018-04-25 15:29:09 -0500

    Add files via upload
    animation added
    Works without background PlayerSprite or Sprite needs to be consolidated and objects need to be called from the player class somehow directly called the objects animate

---
+++ src/BesiegeYourFreinds/src/gfx/BufferedImageLoader.java
@@ -0,0 +1,18 @@
+package gfx;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BufferedImageLoader {
+	
+	public BufferedImage image;
+	
+	public BufferedImage loadImage(String path) throws IOException {
+		
+		image = ImageIO.read(getClass().getResource(path));
+		return image;
+	}
+
+}
