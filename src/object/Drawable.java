d9bbef25ee797bfbc3f407e4a3c10158c9bc47dc
Jamie Guenther <jamieguenther@unomaha.edu>
2018-04-30 22:58:02 -0500

    Delete Drawable.java

--- src/object/Drawable.java
+++
@@ -1,8 +0,0 @@
-package object;
-
-import java.awt.*;
-
-public interface Drawable {
-    void updateWorld(); //Update the World Matrix
-    void render(Graphics g); //Draw the object with passed Graphics
-}
