f1d774a04faf26e6d94e12ec3738fa60ee37efe9
Jamie Guenther <jamieguenther@unomaha.edu>
2018-05-01 16:55:32 -0500

    Hud created.

--- src/BesiegeYourFreinds/BYFApp.java
+++ src/BesiegeYourFreinds/BYFApp.java
@@ -125,6 +125,44 @@ public class BYFApp extends SimpleFramework {
     protected void render(Graphics g) {
 
         bg.renderBG(g);
+        g.setColor(Color.GREEN);
+        g.fillRect(5, 5, player1.health * 2, 25);
+
+        g.setColor(Color.WHITE);
+        g.drawRect(5, 5, 200, 25);
+
+        if(player1.health >= 0) {
+            g.setColor(Color.BLACK);
+            g.drawString("Player 1 Health: " + player1.health  +"%", 5, 45);
+        }
+        else {
+            g.setColor(Color.BLACK);
+            g.drawString("Player 1 Health: " + 0 +"%", 5, 45);
+        }
+
+        g.setColor(Color.BLACK);
+        g.drawString("Angle: " + player1.angle, 5, 60 );
+        g.drawString("Power: " + player1.power, 5, 75 );
+
+        g.setColor(Color.GREEN);
+        g.fillRect(945, 5, player2.health * 2, 25);
+
+        g.setColor(Color.WHITE);
+        g.drawRect(945, 5, 200, 25);
+
+        if(player2.health >= 0) {
+            g.setColor(Color.BLACK);
+            g.drawString("Player 2 Health: " + player2.health  +"%", 945, 45);
+        }
+        else {
+            g.setColor(Color.BLACK);
+            g.drawString("Player 2 Health: " + 0 +"%", 945, 45);
+        }
+
+        g.setColor(Color.BLACK);
+        g.drawString("Angle: " + player2.angle , 945, 60 );
+        g.drawString("Power: " + player2.power , 945, 75 );
+
         player1.render(g);
         player2.render(g);
         testAmmo.render(g);
