f340382e4f72ab5adfb7644ece5ec64bbc546a02
Logan Fitzgibbons <logiwan992@gmail.com>
2018-05-02 00:54:43 -0500

    Added fancier menu with a menu manager

---
+++ src/object/MenuManager.java
@@ -0,0 +1,41 @@
+package object;
+
+import util.Matrix3x3f;
+
+import java.awt.*;
+
+public class MenuManager {
+    private Menu titleScreen;
+    private Menu player1Wins;
+    private Menu player2Wins;
+    public int winner;
+
+    public MenuManager() {
+        titleScreen = new Menu("TitleScreen.png");
+        player1Wins = new Menu("Player1Wins.png");
+        player2Wins = new Menu("Player2Wins.png");
+
+        winner = 0;
+    }
+
+    public void update(float deltaTime, Matrix3x3f viewport) {
+        titleScreen.update(deltaTime, viewport);
+        player1Wins.update(deltaTime, viewport);
+        player2Wins.update(deltaTime, viewport);
+
+    }
+
+    public void render(Graphics g) {
+        if (winner == 0) {
+            titleScreen.render(g);
+        } else if (winner == 1) {
+            player1Wins.render(g);
+        } else if (winner == 2) {
+            player2Wins.render(g);
+        }
+    }
+
+    public void setWinner(int winner) {
+        this.winner = winner;
+    }
+}
